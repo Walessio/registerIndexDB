37902d0d1f00284e58bd4f98c7e5ddfbb5389239
Walessio <alessio.axel@gmail.com>
2017-01-13 22:24:47 +0100

    delete unusued file
    delete unusued file

--- app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('redirect', []);
-app.controller("redirectController", function($window) {
-	function init(){
-	console.log('init');
-	$window.location.href = 'register/index.html';
-	}
-	init();
-});
